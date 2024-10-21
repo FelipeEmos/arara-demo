9808c0a533a49d9da7e97e5ad4fc10ced4f77f84
Felipe Emos <felipe.emos.computacao@gmail.com>
2024-10-21 03:16:52 -0300

    feat: expand body dimension implementations

--- src/lib/composers/subctractive-pass.ts
+++ src/lib/composers/subctractive-pass.ts
@@ -3,42 +3,42 @@ import { Body2DAnimationPass } from "../body-2d-animation";
 import { BodyAnimationPass } from "../body-animation";
 import { Body3DAnimationPass } from "../body-3d-animation";
 
-export function additivePass(rawPass: BodyAnimationPass): BodyAnimationPass {
+export function subtractivePass(rawPass: BodyAnimationPass): BodyAnimationPass {
   return (world) => {
     const { body } = world;
     const rawResult = rawPass(world);
     return {
-      position: body.position + rawResult.position,
-      velocity: body.velocity + rawResult.velocity,
-      acceleration: body.acceleration + rawResult.acceleration,
+      position: body.position - rawResult.position,
+      velocity: body.velocity - rawResult.velocity,
+      acceleration: body.acceleration - rawResult.acceleration,
     };
   };
 }
 
-export function additive2DPass(
+export function subtractive2DPass(
   rawPass: Body2DAnimationPass,
 ): Body2DAnimationPass {
   return (world) => {
     const { body } = world;
     const rawResult = rawPass(world);
     return {
-      position: vec2.add([0, 0], body.position, rawResult.position),
-      velocity: vec2.add([0, 0], body.velocity, rawResult.velocity),
-      acceleration: vec2.add([0, 0], body.acceleration, rawResult.acceleration),
+      position: vec2.sub([0, 0], body.position, rawResult.position),
+      velocity: vec2.sub([0, 0], body.velocity, rawResult.velocity),
+      acceleration: vec2.sub([0, 0], body.acceleration, rawResult.acceleration),
     };
   };
 }
 
-export function additive3DPass(
+export function subtractive3DPass(
   rawPass: Body3DAnimationPass,
 ): Body3DAnimationPass {
   return (world) => {
     const { body } = world;
     const rawResult = rawPass(world);
     return {
-      position: vec3.add([0, 0, 0], body.position, rawResult.position),
-      velocity: vec3.add([0, 0, 0], body.velocity, rawResult.velocity),
-      acceleration: vec3.add(
+      position: vec3.sub([0, 0, 0], body.position, rawResult.position),
+      velocity: vec3.sub([0, 0, 0], body.velocity, rawResult.velocity),
+      acceleration: vec3.sub(
         [0, 0, 0],
         body.acceleration,
         rawResult.acceleration,
